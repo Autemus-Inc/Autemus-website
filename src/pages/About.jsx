fe4c7251ffe98853a2016d4edad5971b2b4049f8
Khemmie-Ray <atokemmy@gmail.com>
2023-10-04 11:45:53 +0100

    fixing build errors

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import logo from "../assets/bg-logo.svg"
+import{ NavLink }from "react-router-dom"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+       <div className='whiteBg'>
+    <header>
+        <img src={logo} alt="" className='logo'/>
+        <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="">Portfolio</NavLink>
+            <NavLink to="">Service</NavLink>
+            <NavLink to="" className="contact-btn">Contact</NavLink>
+        </nav>
+    </header>
+    </div>
+    <main>
+      
+    </main>
+    </div>
   )
 }
 
